322f93e713a3873ef3b99e9d9ce0810113c8b267
Holmesloy <zhangxzloy@163.com>
2020-08-25 10:44:36 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,38 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'blog',
+    title: '进阶',
+    description: '学习笔记',
     base: '/myBlog/',
+    head: [
+      ['link', { rel: 'icon', href: `/cc.png` }],
+    ],
+    themeConfig: {
+      logo: "/cc.png",
+      nav: [
+        { text: '主页', link: '/' },
+        { text: '前端知识', link: '/frontend/' },
+        { text: '算法', link: '/algorithm/' },
+        { text: '笔记', link: '/notes/' },
+        // 下拉列表的配置
+        {
+          text: 'Languages',
+          items: [
+            { text: 'Chinese', link: '/language/chinese' },
+            { text: 'English', link: '/language/English' }
+          ]
+        }
+      ],
+      sidebar: {
+        "/frontend/":[
+          ["", "Hello"]
+        ],
+        "/algorithm/":[
+          ["", "算法"],
+          ["offer","剑指offer题解"],
+          ["leetcode", "leetcode题解"]
+        ],
+        "/notes/": [
+          ["", "笔记"]
+        ],
+      }
+    }
   }
---
+++ docs/frontend/README.md
@@ -0,0 +1,1 @@
+Hi，this is frontend page
